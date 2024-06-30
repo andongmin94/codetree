9c4814629bd5e63cac58d12f262dca6b7775fa80
안동민 <andongmin94@gmail.com>
2024-06-30 22:25:53 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240630/그 계절, 그 날/that-season-that-day.cpp
+++ 240630/그 계절, 그 날/that-season-that-day.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-// 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
-int LastDayNumber(int m) {
+int LastDayNumber(int y, int m) {
+    if(y % 4 == 0 && m == 2)
+        return 29;
     if(m == 2)
         return 28;
     if(m == 4 || m == 6 || m == 9 || m == 11)
@@ -12,9 +13,8 @@ int LastDayNumber(int m) {
     return 31;
 }
 
-// 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
 bool JudgeDay(int y, int m, int d) {
-    if(m <= 12 && d <= LastDayNumber(m))
+    if(m <= 12 && d <= LastDayNumber(y, m))
         return true;
     
     return false;
