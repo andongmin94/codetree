2e9bc25fe89f5fd0823d9d85de6209c56ca9f8d0
안동민 <andongmin94@gmail.com>
2024-07-06 03:19:01 +0900

    Title: 반복 출력하기 2, Time: 1096ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

---
+++ 240706/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void func(int n)
+{
+    if (n > 0)
+        cout << "HelloWorld\n";
+    func(n-1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    func(n);
+}
