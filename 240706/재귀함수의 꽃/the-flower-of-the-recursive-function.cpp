a1b2065f9571a53965129001863feca5217dd04f
안동민 <andongmin94@gmail.com>
2024-07-06 20:49:43 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240706/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void func(int n)
+{
+    if (n == 0) return;
+    cout << n << ' ';
+    func(n - 1);
+    cout << n << ' ';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    func(n);
+}
