8ed110f0542de5d2f4d85f003f4170c42fca0e7f
안동민 <andongmin94@gmail.com>
2024-07-07 02:29:04 +0900

    Title: 1이 되는 순간까지, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240707/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 240707/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -6,12 +6,14 @@ int func(int n, int m)
 {
     if (n == 1) return m;
 
-    if (n % 2 == 0) func(n / 2, m + 1);
-    else func(n / 3, m + 1);
+    if (n % 2 == 0) return func(n / 2, m + 1);
+    else return func(n / 3, m + 1);
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int n;
     cin >> n;
