eb747b61fb8978fa1c41be7d06ab67544fdad73e
안동민 <andongmin94@gmail.com>
2024-07-09 08:36:15 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240709/오름 내림차순 정렬/inc-dec-sorting.cpp
+++ 240709/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -13,7 +13,11 @@ int main()
     
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    sort(arr, arr + n, greater<int>());
+    sort(arr, arr + n);
     for (int i = 0; i < n; i++)
         cout << arr[i] << ' ';
+    cout << '\n';
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << ' ';
+    
 }
