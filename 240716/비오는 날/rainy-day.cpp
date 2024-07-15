296830691d66a3a09fcc0ce884462821b62d9c71
안동민 <andongmin94@gmail.com>
2024-07-16 00:37:24 +0900

    Title: 비오는 날, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/비오는 날/rainy-day.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Rainy
+{
+public:
+    string day;
+    string week;
+    string sky;
+
+    Rainy(string day = "", string week = "", string sky = "")
+    {
+        this->day = day;
+        this->week= week;
+        this->sky = sky;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    Rainy rainy[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> rainy[i].day >> rainy[i].week >> rainy[i].sky;
+    }
+
+    int idx = 0;
+    string s = "9999-99-99";
+    for (int i = 1; i < n; i++)
+    {
+        if (rainy[i].sky == "Rain" && rainy->day < s)
+        {
+            s = rainy->day;
+            idx = i;
+        }
+    }
+    cout << rainy[idx].day << ' ' << rainy[idx].week << ' ' << rainy[idx].sky;
+}
