284bb432d71917ed76fd50c83073b8481847ff50
안동민 <andongmin94@gmail.com>
2024-07-18 01:11:59 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/개인정보/personal-info.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Human
+{
+public:
+    string name;
+    int height;
+    float weight;
+
+    Human(string name = "", int height = 0, float weight = 0)
+    {
+        this->name = name;
+        this->height = height;
+        this->weight = weight;
+    }
+};
+
+bool cmpName(Human a, Human b)
+{
+    return a.name < b.name;
+}
+
+bool cmpHeight(Human a, Human b)
+{
+    return a.height > b.height;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Human mans[5];
+
+    for (int i = 0; i < 5; i++)
+        cin >> mans[i].name >> mans[i].height >> mans[i].weight;
+
+    sort(mans, mans + 5, cmpName);
+
+    cout << "name\n";
+    for (int i = 0; i < 5; i++)
+        cout << mans[i].name << ' ' <<  mans[i].height << ' ' << mans[i].weight << "\n";
+
+    sort(mans, mans + 5, cmpHeight);
+    cout << "\nheight\n";
+    for (int i = 0; i < 5; i++)
+        cout << mans[i].name << ' ' <<  mans[i].height << ' ' << mans[i].weight << "\n";
+}
